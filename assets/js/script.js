063cbb1c14bdf546fa1e8f00d5febe470aa6c0a9
Keita Yagi <kei.yagi@gmail.com>
2016-02-04 03:14:07 +0900

    Change the logic type to Object orientation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,34 +6,38 @@ $(function(){
   var input_data,
       result = false;
 
+  function SearchAPI(){
+  }
+  SearchAPI.prototype = {
+  	search: function(){
+  	  var me = this;
+      $.ajax({
+        method: 'GET',
+        url: '../assets/model/category.json',
+        dataType: 'json'
+      }).done(function(json){
+  	    me.scan(json);
+      });
+  	},
+  	scan: function(data){
+      for(key in data){
+  	    // data[key].categories: array
+  	    for(var i = 0; i < data[key].categories.length; i++){
+  	  	  if(input_data === data[key].categories[i].id){
+  	  	    type.html(data[key].name);
+  	  	    cat.html(data[key].categories[i].name);
+  	  	  }
+  	    }
+  	  }
+  	}
+  };
+
+  var API = new SearchAPI();
+
   btn.on('click', function(e){
   	e.preventDefault();
   	input_data = txt.val();
-  	getData();
+  	API.search();
   });
-
-  function getData(){
-    $.ajax({
-      method: 'GET',
-      url: '../assets/model/category.json',
-      dataType: 'json'
-    }).done(function(json){
-  	  console.log(json);
-  	  ScanCat(json)
-    });
-  }
-
-  function ScanCat(data){
-  	for(key in data){
-  	  // data[key].categories: array
-  	  for(var i = 0; i < data[key].categories.length; i++){
-  	  	if(input_data === data[key].categories[i].id){
-  	  	  type.html(data[key].name);
-  	  	  cat.html(data[key].categories[i].name);
-  	  	}
-  	  }
-  	}
-  }
-
 
 });
